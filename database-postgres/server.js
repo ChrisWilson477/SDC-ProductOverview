c221d808ece965da49327ba1fc5203bf0befa23f
Chris <68999991+ChrisWilson477@users.noreply.github.com>
2021-03-12 18:54:08 -0700

    started ReadMe, updated package.json

--- database-postgres/server.js
+++ database-postgres/server.js
@@ -7,20 +7,9 @@ const cors = require('cors');
 app.use(express.static('../client/dist')); // Host your dist folder up to the server
 app.use(express.json()); // Alternative to BodyParser
 
-<<<<<<< Updated upstream
 app.get('/products/list', db.getProductList);
 app.get('/products/:product_id', db.getSingleProduct);
 app.get('/products/:product_id/styles', db.getSingleProductStyles);
-=======
-
-
-app.get('http://52.26.193.201:3000/products/list', db.getProductList)
-app.get('http://52.26.193.201:3000/products/:product_id', db.getSingleProduct)
-app.get('http://52.26.193.201:3000/products/:product_id/styles', db.getSingleProductStyles)
-
-
-
->>>>>>> Stashed changes
 
 // Listening for requests on the PORT
 app.listen(PORT, () => {
