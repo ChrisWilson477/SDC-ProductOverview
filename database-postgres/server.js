f0fe93a08c04acc73013efa1eac247c4ee445c38
Chris <68999991+ChrisWilson477@users.noreply.github.com>
2021-03-13 17:19:17 -0700

    updated styles route

--- database-postgres/server.js
+++ database-postgres/server.js
@@ -7,40 +7,42 @@ const cors = require('cors');
 app.use(express.static('../client/dist')); // Host your dist folder up to the server
 app.use(express.json()); // Alternative to BodyParser
 
-
-
 //Routes
 //Product List
 app.get('/products/list', async (req, res) => {
-    try {
-      let productList = await db.getProductList();
-        res.status(200).json(productList);
-    } catch (err) {
-        console.log(err.message)
-    }
+  try {
+    let productList = await db.getProductList();
+    res.status(200).json(productList);
+  } catch (err) {
+    console.log(err.message);
+  }
 });
-
 
 //Single Product with Features and Values
 app.get('/products/list/:product_id', async (req, res) => {
-    try {
-      let {product_id} = req.params;
-      let singleProduct = await db.getSingleProduct(product_id);
-        res.status(200).json(singleProduct);
-    } catch (err) {
-        console.log(err.message)
-    }
+  try {
+    let { product_id } = req.params;
+    let singleProduct = await db.getSingleProduct(product_id);
+    res.status(200).json(singleProduct);
+  } catch (err) {
+    console.log(err.message);
+  }
 });
 
-
-app.get('/products/:product_id/styles', db.getSingleProductStyles);
-
-
-
-
-
+//Single Product Styles
+app.get('/products/:product_id/styles', async (req, res) => {
+  try {
+    let { product_id } = req.params;
+    let singleProductStyles = await db.getSingleProductStyles(
+      product_id,
+    );
+    res.status(200).json(singleProductStyles);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
 // Listening for requests on the PORT
 app.listen(PORT, () => {
-	console.log(`Serving up now at ${PORT}`);
+  console.log(`Serving up now at ${PORT}`);
 });
